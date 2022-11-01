c063451c989e791c3d7b2b4244518973dd496ca8
cueo <mohit.ritanil@gmail.com>
2022-11-01 01:40:35 -0400

    Add utility methods

---
+++ util.py
@@ -0,0 +1,24 @@
+import random
+
+from const import CLIENT_ID, VERSION
+from log import get_logger
+
+log = get_logger(__name__)
+
+
+def bytes_to_str(b: bytes) -> str:
+    return b.decode('utf-8')
+
+
+def generate_id() -> bytes:
+    """
+    Generates an Azureus-style peer_id.
+    Concatenates client id, version and a 12 digit random number.
+
+    Returns: new peer_id
+    """
+    n = 12
+    random_num = random.randint(10 ** (n-1), (10 ** n) - 1)
+    _id = f'-{CLIENT_ID}{VERSION}-{random_num}'
+    log.info(f'Generated {_id=}')
+    return _id.encode(encoding='utf-8')
